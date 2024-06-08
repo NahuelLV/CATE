49dfcacc9920d30f27c923e96fd6e1a981896b0b
NahuelLV <130933110+NahuelLV@users.noreply.github.com>
2024-06-08 16:29:34 -0300

    Update Ejercicio6(Clase5).c

--- Ejercicio6(Clase5).c
+++ Ejercicio6(Clase5).c
@@ -13,6 +13,7 @@ int main() {
     int alumMAX;
     int desaprobados = 0;
     float porcentajeD;
+    float sumadenotas;
     for(int i = 0; i < cantidadA; i++){
     printf("Ingrese la nota del alumno: ");
     scanf("%f", &notas[i]);
@@ -26,10 +27,20 @@ int main() {
         notaMAX = notas[i];
         alumMAX = i+1;
     }
+    sumadenotas += notas[i];
     }
     printf("La cantidad de alumnos aprobados es de: %.d\n", aprobados);
     porcentajeD = (desaprobados * 100) /cantidadA;
     printf("El porcentaje de desaprobados es de: %.2f\n", porcentajeD);
     printf("El alumno numero %d tuvo la nota mas alta, y fue: %.2f\n", alumMAX, notaMAX);
+    
+    float promedio = sumadenotas / cantidadA;
+    int menorP;
+    for(int j = 0; j < cantidadA; j++){
+        if(notas[j] < promedio)
+        menorP += 1;
+    }
+    
+    printf("Hay %d notas menores al promedio total(%.2f)", menorP, promedio);
     return 0;
 }
