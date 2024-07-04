51688f41aa2aa781dc4dd352385dfc5d0a16f338
NahuelLV <130933110+NahuelLV@users.noreply.github.com>
2024-07-03 21:05:39 -0300

    Create Clase7.c

---
+++ Clase7.c
@@ -0,0 +1,26 @@
+/* Ejercicio 1: realizar un programa para imprimir el ticket de un supermarcado, se lee el precio de cada producto y la cantidad. Finaliza el programa al ingresar 0 como cantidad. Mostrar precio promedio por producto(todos los precios / los productos), total a pagar, precio del producto con mayor cantidad.
+
+Ejercicio 2: realizar un programa que procese los datos de las personas que van a natacion. Se leen la edad y el nivel de cada asistente(1 = AVANZADO - 2 = INTERMEDIO - 3 = BASICO). Se cargan datos hasta que se ingrese una edad negativa y se admiten hasta 10 nadadores. Puede ser que no se presente nadie. Mostrar cuantos tienen nivel avanzado. Expresar en porcentaje. Promedio de edades de nivel basico. Porcentaje entre los de nivel basico que son mayores de edad. Edad minima y a que nivel asiste.
+
+
+Tarea: Cargar en una matriz cargar las notas de 3 alumnos en 3 materias. Mostrar
+el promedio de cada alumno, promedio por materia, porcentaje de notas aprobadas
+diagonal principal y nota maxima con su estudiante y en que materia.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    
+    int cantidad = 0;
+    int producto = 0;
+    int ticket[n][2];
+    int n = 0;
+    
+    printf("Ingrese la cantidad de productos: ")
+    scanf("%d\n", &n);
+    
+    return 0;
+}
